46d84431b63a85945d285f80ec4ca509d2d0d3e9
Kelly Raila <kelly.raila@gmail.com>
2015-06-06 21:35:52 -0400

    red

--- lib/container.rb
+++ lib/container.rb
@@ -1,3 +1,7 @@
 class Container
-
+  def initialize(name,weight,capacity)
+    @name = name
+    @weight = weight
+    @capacity = capacity
+  end
 end
--- spec/lib/container_spec.rb
+++ spec/lib/container_spec.rb
@@ -7,6 +7,12 @@ describe Container do
     it 'has a type Cheesy Poofs' do
       expect(container.type).to eq('Cheesy Poofs')
     end
+    it 'has a weight of 50' do
+      expect(container.weight).to eq(50)
+    end
+    it 'has a capacity of 20' do
+      expect(container.capacity).to eq(20)
+    end
   end
 
 end
